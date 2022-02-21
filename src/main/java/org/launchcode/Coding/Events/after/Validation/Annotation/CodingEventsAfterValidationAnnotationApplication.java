8e22a8404c9fdaec4843741f8d0970e6482d1bb9
1mbalducci <michelle.balducci@gmail.com>
2022-02-21 08:30:53 -0600

    tryOne

---
+++ src/main/java/org/launchcode/Coding/Events/after/Validation/Annotation/CodingEventsAfterValidationAnnotationApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Coding.Events.after.Validation.Annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingEventsAfterValidationAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingEventsAfterValidationAnnotationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Coding/Events/after/Validation/Annotation/CodingEventsAfterValidationAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Coding.Events.after.Validation.Annotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingEventsAfterValidationAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
